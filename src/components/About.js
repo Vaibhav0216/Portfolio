dfd746b93eaf620a4da189ea58478b890c0118ae
Vaibhav02 <maulikshirsagarvk18@gmail.com>
2024-06-08 23:40:20 +0530

    Added contact information

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
                     <h2>Vaibhav Kshirsagar</h2>
                     <p className="description">As a graduate student in Electronics and Telecommunication, I am deeply passionate about software development. My professional experience as an embedded software developer has provided me with valuable skills and insights, further fueling my enthusiasm for this field. I am eager to leverage my background and expertise to pursue opportunities in software development and contribute meaningfully to innovative projects.</p>
                     <a href="https://drive.google.com/file/d/1nRRPgzxVnbGPzaQocfbE3QpY6T67ztXk/view" target="_blank" rel="noopener noreferrer">
-                        <button className="hire-button">Hire Me</button>
+                        <button className="hire-button">Download Resume</button>
                     </a>
                 </div>
                 <div className="profile-image-container">
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,104 @@
+// import React, { useEffect } from 'react';
+// import './Contact.css';
+
+// const Contact = ({ isDarkMode }) => { 
+//     useEffect(() => {
+//         const h2Element = document.querySelector('#contact h2');
+//         if (isDarkMode) {
+//             h2Element.style.color = 'white';
+//         } else {
+//             h2Element.style.color = 'black';
+//         }
+//     }, [isDarkMode]);
+
+//     return (
+//         <section id="contact">
+//             <h2>Contact</h2>
+//             <form>
+//                 <label>
+//                     Name:
+//                     <input type="text" name="name" />
+//                 </label>
+//                 <label>
+//                     Email:
+//                     <input type="email" name="email" />
+//                 </label>
+//                 <label>
+//                     Message:
+//                     <textarea name="message"></textarea>
+//                 </label>
+//                 <button type="submit">Submit</button>
+//             </form>
+//         </section>
+//     );
+// }
+
+// export default Contact;
+// import React, { useEffect } from 'react';
+// import './Contact.css';
+
+// const Contact = ({ isDarkMode }) => {
+//     useEffect(() => {
+//         const h2Element = document.querySelector('#contact h2');
+//         if (isDarkMode) {
+//             h2Element.style.color = 'white';
+//         } else {
+//             h2Element.style.color = 'black';
+//         }
+//     }, [isDarkMode]);
+
+//     return (
+//         <section id="contact">
+//             <h2>Contact</h2>
+//             <div className="contact-form">
+//             <div className="contact-info">
+//                 <h3>Location</h3>
+//                 <p>Janwadi, Gokhalenagar, Pune, Maharashtra 411016</p>
+//                 <h3>Phone</h3>
+//                 <p>+91 7498236616</p>
+//                 <h3>Email</h3>
+//                 <p>maulikshirsagarvk18@gmail.com</p>
+//                 <h3>Map</h3>
+//                 <iframe 
+//                     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3782.969212129287!2d73.8222953751919!3d18.530293382565215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDMxJzQ5LjEiTiA3M8KwNDknMjkuNSJF!5e0!3m2!1sen!2sin!4v1717828677205!5m2!1sen!2sin" 
+//                     width="100%" 
+//                     height="100%" 
+//                     style={{ border: 0 }} 
+//                     allowFullScreen="" 
+//                     loading="lazy" 
+//                     referrerPolicy="no-referrer-when-downgrade">
+//                 </iframe>
+//             </div>
+//             <form>
+//                 <label>
+//                     Name:
+//                     <input type="text" name="name" />
+//                 </label>
+//                 <label>
+//                     Email:
+//                     <input type="email" name="email" />
+//                 </label>
+//                 <label>
+//                     Subject:
+//                     <input type="text" name="subject" />
+//                 </label>
+//                 <label>
+//                     Message:
+//                     <textarea name="message"></textarea>
+//                 </label>
+//                 <button type="submit">Submit</button>
+//             </form>
+//             </div>
+//         </section>
+//     );
+// }
+
+// export default Contact;
+
 import React, { useEffect } from 'react';
 import './Contact.css';
 
-const Contact = ({ isDarkMode }) => { 
+const Contact = ({ isDarkMode }) => {
     useEffect(() => {
         const h2Element = document.querySelector('#contact h2');
         if (isDarkMode) {
@@ -14,21 +111,45 @@ const Contact = ({ isDarkMode }) => {
     return (
         <section id="contact">
             <h2>Contact</h2>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Email:
-                    <input type="email" name="email" />
-                </label>
-                <label>
-                    Message:
-                    <textarea name="message"></textarea>
-                </label>
-                <button type="submit">Submit</button>
-            </form>
+            <div className="contact-form">
+                <div className="contact-info">
+                    <h3>Location</h3>
+                    <p>Janwadi, Gokhalenagar, Pune, Maharashtra 411016</p>
+                    <h3>Phone</h3>
+                    <p>+91 7498236616</p>
+                    <h3>Email</h3>
+                    <p>maulikshirsagarvk18@gmail.com</p>
+                    <h3>Map</h3>
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3782.969212129287!2d73.8222953751919!3d18.530293382565215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDMxJzQ5LjEiTiA3M8KwNDknMjkuNSJF!5e0!3m2!1sen!2sin!4v1717828677205!5m2!1sen!2sin" 
+                        width="100%" 
+                        height="100%" 
+                        style={{ border: 0 }} 
+                        allowFullScreen="" 
+                        loading="lazy" 
+                        referrerPolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+                <form>
+                    <label>
+                        Name:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        Email:
+                        <input type="email" name="email" />
+                    </label>
+                    <label>
+                        Subject:
+                        <input type="text" name="subject" />
+                    </label>
+                    <label>
+                        Message:
+                        <textarea name="message"></textarea>
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </section>
     );
 }
