6e8540e82ffedcdaf1bc63483c8fcdd407247826
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2024-08-20 10:55:52 +0530

    Drive link change

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ const About = () => {
             <div className="about-content">
                 <div className="about-details">
                     <h2>Vaibhav Kshirsagar</h2>
-                    <p className="description">As a graduate student in Electronics and Telecommunication, I am deeply passionate about software development. My professional experience as an embedded software developer has provided me with valuable skills and insights, further fueling my enthusiasm for this field. I am eager to leverage my background and expertise to pursue opportunities in software development and contribute meaningfully to innovative projects.</p>
-                    <a href="https://drive.google.com/file/d/1nRRPgzxVnbGPzaQocfbE3QpY6T67ztXk/view" target="_blank" rel="noopener noreferrer">
+                    <p className="description">As a graduate student in Electronics and Telecommunication, I am deeply passionate about software development. My professional experience as an software developer intern has provided me with valuable skills and insights, further fueling my enthusiasm for this field. I am eager to leverage my background and expertise to pursue opportunities in software development and contribute meaningfully to innovative projects. Currently I working as a graduate Engeneering Trainee in the Amset Technology.</p>
+                    <a href="https://drive.google.com/file/d/1wD7khEY3i4qm_U5LDAeOIBDqtS12tZZm/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                         <button className="hire-button">Download Resume</button>
                     </a>
                 </div>
