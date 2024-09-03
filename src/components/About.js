1bbe796caca0cc96ddc8852bf0acdf1bda381d5d
Vaibhav0216 <maulikshirsagarvk18@gmail.com>
2024-09-03 19:28:40 +0530

    Resume changed

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <div className="about-details">
                     <h2>Vaibhav Kshirsagar</h2>
                     <p className="description">As a graduate student in Electronics and Telecommunication, I am deeply passionate about software development. My professional experience as an software developer intern has provided me with valuable skills and insights, further fueling my enthusiasm for this field. I am eager to leverage my background and expertise to pursue opportunities in software development and contribute meaningfully to innovative projects. Currently I working as a graduate Engeneering Trainee in the Amset Technology.</p>
-                    <a href="https://drive.google.com/file/d/1wD7khEY3i4qm_U5LDAeOIBDqtS12tZZm/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1Mg6jzRlTYUrE8-zwNYm5V8uHsTr1ALSv/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                         <button className="hire-button">Download Resume</button>
                     </a>
                 </div>
